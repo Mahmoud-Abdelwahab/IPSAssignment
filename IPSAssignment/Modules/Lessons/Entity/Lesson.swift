354dc29da30b91e8bc67565c092f33ecc73669f3
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2023-03-22 15:13:58 +0200

    Squashed commit of the following:
    commit 3a603508e92c318a2387fc2ad748ea0af1d8b729
    Author: Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
    Date:   Wed Mar 22 15:08:32 2023 +0200
    
        adding main lesson entity

---
+++ IPSAssignment/Modules/Lessons/Entity/Lesson.swift
@@ -0,0 +1,21 @@
+//
+//  Lesson.swift
+//  IPSAssignment
+//
+//  Created by Mahmoud Abdulwahab on 22/03/2023.
+//
+
+import Foundation
+
+/// This struct represent the main lesson entity
+///
+struct Lesson: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let thumbnailURL: URL
+    let videoURL: URL
+    /// `isVideoCashed`  Identify if the lesson video dowloaded to files or not
+    ///
+    let isVideoCashed = false
+}
